e24401761b66854435f411e0d4cc76036ffa6275
bigbits <bigbits@hackx.cc>
2019-09-24 15:57:11 +0800

    updata.

--- include/global_build_info_time.h
+++ include/global_build_info_time.h
@@ -3,11 +3,11 @@
 
 #define BUILD_TIME_YEAR   2019
 #define BUILD_TIME_MONTH  9
-#define BUILD_TIME_DAY    17
-#define BUILD_TIME_HOUR   13
-#define BUILD_TIME_MINUTE 49
-#define BUILD_TIME_SECOND 22
+#define BUILD_TIME_DAY    24
+#define BUILD_TIME_HOUR   15
+#define BUILD_TIME_MINUTE 53
+#define BUILD_TIME_SECOND 33
 #define BUILD_TIME_WEEK_OF_DAY 1
-#define BUILD_TIME_YEAR_OF_DAY 260
+#define BUILD_TIME_YEAR_OF_DAY 267
 
 //compile append define end
--- include/global_build_info_version.h
+++ include/global_build_info_version.h
@@ -3,9 +3,9 @@
 
 #define BUILD_VERSION_MAJOR   0
 #define BUILD_VERSION_MINOR   1
-#define BUILD_VERSION_MICRO   14
+#define BUILD_VERSION_MICRO   17
 #define BUILD_VERSION_DEV     0
-#define BUILD_GIT_COMMIT_ID   "g10a38f6"
+#define BUILD_GIT_COMMIT_ID   "ge6f0b23"
 #define BUILD_GIT_IS_DIRTY    1
 
 //compile append define end
